d1c60cb94a17fe98393d14bb90e7a30ec89ab4b2
Eduardo De La Cruz <eduardo.delacruz@gft.com>
2018-11-20 13:36:49 +0100

    Initial Commit

---
+++ JsonDecodeSwift4Vs4_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JsonDecodeSwift4Vs4_1
+//
+//  Created by De La Cruz, Eduardo on 20/11/2018.
+//  Copyright © 2018 De La Cruz, Eduardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
